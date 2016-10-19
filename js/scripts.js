ea61c575904243eb38d0596b46cdb1336f884759
Alex Francois and Brad Copenhaver <alex@>
2016-10-19 15:01:01 -0700

    add html, js framework for marketplace order form

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $("form#name").submit(function(event){
+    event.preventDefault();
+    var firstName = $("input#firstNameInput").val();
+    var lastName = $("input#lastNameInput").val();
+    var street = $("input#streetInput").val();
+    var city = $("input#cityInput").val();
+    var state = $("input#stateInput").val();
+    var zip = $("input#zipInput").val();
+
+    $("")
+
+
+  })
+})
